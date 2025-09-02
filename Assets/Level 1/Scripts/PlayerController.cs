605abed4993f31d41fee2e4402152c384f847734
Andrea Cubillos <Andreav.cubillosp@konradlorenz.edu.co>
2025-09-02 13:21:07 -0500

    Implementación de trampas, plataformas y ambientación sonora
    Se agregaron dos trampas (una de lodo y otra con un rastrillo) junto con su lógica. Se implementó la lógica para que las tres plataformas desaparezcan y reproduzcan un sonido al hacerlo. Además, se añadió sonido ambiental para ambos niveles y un objeto decorativo (orquídea) para enriquecer la escena.

--- Assets/Level 1/Scripts/PlayerController.cs
+++ Assets/Level 1/Scripts/PlayerController.cs
@@ -1,32 +1,91 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
-    private Vector3 posicion;
-    // Start is called before the first frame update
+    public bool enterrado = false;
+    public float profundidadEnterrado = -0.5f; // Valor negativo para enterrar
+    private Vector3 posicionOriginal;
+    public AudioSource sonido1;
+    private bool sonidoReproducido = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        posicionOriginal = transform.position; // Guarda la posición inicial
 
+
+        sonido1 = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        if (!enterrado)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movimiento * speed);
+        }
     }
 
-        void FixedUpdate()
+    void OnTriggerEnter(Collider other)
     {
-        // se ejcuta muchas veces por segundo
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        
-        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movimiento * speed);
-    }
+        if (other.gameObject.CompareTag("Recolectable"))
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Mud") )
+        {
+            enterrado = true;
+            // Mueve al jugador hacia abajo (enterrado)
+            transform.Translate(0, profundidadEnterrado, 0);
+            // Detiene el Rigidbody
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.isKinematic = true; // Evita que se mueva por física
+
+                  // Mensaje en la consola
+            Debug.Log("¡Ups! Has quedado enterrado en el lodo");
+        }
+
+        if (other.gameObject.CompareTag("Rake"))
+        {
+            enterrado = true;
+            transform.Translate(0, profundidadEnterrado, 0);
+            // Opcional: Detener el Rigidbody
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            Debug.Log("¡Te has pinchado con el rastrillo!");
+        }
+
+
+        if (other.gameObject.CompareTag("Desaparecer") && !sonidoReproducido)
+        {
+            // Destruye todas las plataformas
+            GameObject[] plataformas = GameObject.FindGameObjectsWithTag("Platform");
+            foreach (GameObject plataforma in plataformas)
+            {
+                Destroy(plataforma);
+            }
+
+            Debug.Log("¡Todas las plataformas han sido destruidas!");
+
+            // Reproduce el sonido SOLO al destruir las plataformas
+            if (sonido1 != null)
+            {
+                sonido1.Play();
+                sonidoReproducido = true; // Marca el sonido como reproducido
+            }
+
+    
+
+
+
+    
+}
+}
 }
