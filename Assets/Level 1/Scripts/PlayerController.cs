4ba12a6fc7b0387ebcef7486964e2f025411977f
Andrea Cubillos <Andreav.cubillosp@konradlorenz.edu.co>
2025-09-01 21:59:37 -0500

    Avance en la maqueta del segundo nivel y configuración de scripts
    Se completó parte de la maqueta del segundo nivel. Se agregaron los scripts PlayerController para permitir el movimiento de la esfera, CameraController para que la cámara siga al jugador, y Rotator para que las fresas recolectables roten en su propio eje. Además, se ajustaron los colliders de los arbustos para evitar que la esfera (jugador) los atraviese.

---
+++ Assets/Level 1/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private Vector3 posicion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+        void FixedUpdate()
+    {
+        // se ejcuta muchas veces por segundo
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movimiento * speed);
+    }
+}
